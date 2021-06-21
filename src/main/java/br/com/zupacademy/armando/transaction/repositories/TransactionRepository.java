195117a91e2d459906182e41bf3f89757d7e56b7
Armando Tadeu <atdc.codemaster@gmail.com>
2021-06-21 12:44:10 -0300

    feature: 11.autenticacao_autorizacao

--- src/main/java/br/com/zupacademy/armando/transaction/repositories/TransactionRepository.java
+++ src/main/java/br/com/zupacademy/armando/transaction/repositories/TransactionRepository.java
@@ -2,7 +2,6 @@ package br.com.zupacademy.armando.transaction.repositories;
 
 import br.com.zupacademy.armando.transaction.entities.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,7 +12,6 @@ public interface TransactionRepository extends JpaRepository<Transaction, String
 
     Optional<Transaction> findFirstByCardId(String cardId);
 
-    //@Query("select t from Transaction t where t.cardId = :cardId order by t.effectIn desc limit :limit")
     List<Transaction> findTop10ByCardIdOrderByEffectInDesc(String cardId);
 
 }
